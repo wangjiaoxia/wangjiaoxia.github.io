b8a2e40cc04120c3fc391ff84caa995604a3e51a
wangjiaoixa <jiaoxia13@163.com>
2016-12-14 13:51:05 +0800

    test qrcode table

--- js/qrcode.js
+++ js/qrcode.js
@@ -15,7 +15,7 @@ $(document).ready(function($) {
         });
     }
 
-    getQRCode("canvas", 200, 200, "https://wangjiaoxia.github.io/");
+    getQRCode("table", 100, 100, "https://wangjiaoxia.github.io/");
 
     //中文编码格式转换
     function utf16to8(str) {
